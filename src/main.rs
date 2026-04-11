ee76b85911f65835001c54e73d27a82ee28d9b7e
Snowdeenn <legoffelouan05@gmail.com>
2026-04-11 18:24:31 +0200

    affichage du triangle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glow::{HasContext};
+use glow::HasContext;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::raw_window_handle::HasWindowHandle;
@@ -12,8 +12,10 @@ use glutin::surface::{Surface, SurfaceAttributesBuilder};
 
 use glutin_winit::{DisplayBuilder, GlWindow};
 
+use core::f32;
 use std::time::Instant;
-
+use std::mem::size_of;
+use std::slice::{from_raw_parts};
 fn main() {
     let event_loop: EventLoop<()> = EventLoop::new().unwrap();
     
@@ -67,9 +69,75 @@ fn main() {
         })
     };
 
-    let time = Instant::now();
+    let time: Instant = Instant::now();
+
+    unsafe {
+        // ------ Vertex Shader ------
+        let vertex_shader: glow::NativeShader = gl.create_shader(glow::VERTEX_SHADER)
+            .unwrap();
+        gl.shader_source(vertex_shader, include_str!("shader/vertex.glsl"));
+        gl.compile_shader(vertex_shader);
+        
+        if !gl.get_shader_compile_status(vertex_shader) {
+            panic!("[Vertex_shader] Erreur : {}", gl.get_shader_info_log(vertex_shader));
+        }
+
+        // ------ Fragement Shader ------
+        let frag_shader: glow::NativeShader = gl.create_shader(glow::FRAGMENT_SHADER)
+            .unwrap();
+        gl.shader_source(frag_shader, include_str!("shader/fragement.glsl"));
+        gl.compile_shader(frag_shader);
+
+        if !gl.get_shader_compile_status(frag_shader) {
+            panic!("[Frag_shader] Erreur : {}", gl.get_shader_info_log(frag_shader));
+        }
+
+        // -------- Programme --------
+        let program: glow::NativeProgram = gl.create_program().unwrap();
+        gl.attach_shader(program, vertex_shader);
+        gl.attach_shader(program, frag_shader);
+        gl.link_program(program);
+
+        if !gl.get_program_link_status(program) {
+            panic!("[Program] Erreur : {}", gl.get_program_info_log(program));
+        }
+
+        // -------- Netoyage --------
+        gl.detach_shader(program, vertex_shader);
+        gl.detach_shader(program, frag_shader);
+        gl.delete_shader(vertex_shader);
+        gl.delete_shader(frag_shader);
+
+        // -------- Mise en place VAO et VBO --------
+        let vao: glow::NativeVertexArray = gl.create_vertex_array().unwrap();
+        let vbo: glow::NativeBuffer = gl.create_buffer().unwrap();
+
+        gl.bind_vertex_array(Some(vao));
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+
+        let vertices: [f32; 9] = [
+            -0.5, -0.5, 0.0,
+            0.5, -0.5, 0.0,
+            0.0, 0.5, 0.0
+        ];
+
+        // -------- Remplissage du VBO --------
+        let vertice_ptr: *const u8 = &vertices as *const f32 as *const u8;
+        let vertice_size = vertices.len() * size_of::<f32>();
+
+        gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, from_raw_parts(vertice_ptr, vertice_size), glow::STATIC_DRAW);
     
-    event_loop.run(move |event: Event<()>, window_target:&winit::event_loop::ActiveEventLoop| {
+        // -------- Description pour le vao --------
+        let stride: i32 = 3 * size_of::<f32>() as i32;
+        let offset: i32 = 0; // Car on a que la position en attribut
+
+        gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, stride, offset);
+        gl.enable_vertex_attrib_array(0);
+
+        gl.bind_vertex_array(None);
+        gl.bind_buffer(glow::ARRAY_BUFFER, None);
+
+        event_loop.run(move |event: Event<()>, window_target:&winit::event_loop::ActiveEventLoop| {
         window_target.set_control_flow(ControlFlow::Poll);
         match event {
 
@@ -78,17 +146,19 @@ fn main() {
                 match event {
                     WindowEvent::CloseRequested => window_target.exit(),
                     WindowEvent::RedrawRequested => {
-                        let current_time = time.elapsed().as_secs_f32();
+                        let current_time: f32 = time.elapsed().as_secs_f32();
 
-                        let r = (current_time.sin() + 1.0) / 2.0;
-                        let g = (current_time.cos() + 1.0) / 2.0;
-                        let b = (current_time.sin() + 1.0) / 2.0;
+                        let r: f32 = (current_time.sin() + 1.0) / 2.0;
+                        let g: f32 = (current_time.cos() + 1.0) / 2.0;
+                        let b: f32 = (current_time.sin() + 1.0) / 2.0;
 
-                        unsafe {
-                            gl.clear_color(r, g, b, 1.0);
-                            gl.clear(glow::COLOR_BUFFER_BIT);
-                            gl_surface.swap_buffers(&gl_context).unwrap();
-                        };
+                        gl.clear_color(r, g, b, 1.0);
+                        gl.clear(glow::COLOR_BUFFER_BIT);
+                        gl.use_program(Some(program));
+                        gl.bind_vertex_array(Some(vao));
+                        gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
+                        gl_surface.swap_buffers(&gl_context).unwrap();
                         
                     },
                     _ => ()
@@ -97,4 +167,6 @@ fn main() {
             _ => window.request_redraw(),
         }
     }).unwrap();
+    };
+    
 }
